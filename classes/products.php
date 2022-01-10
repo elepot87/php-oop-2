b0c8f259bce664a2a40c39aa8a0529848435d1d8
elepot87 <78643839+elepot87@users.noreply.github.com>
2022-01-10 14:24:51 +0100

    creata una classe principale Product

---
+++ classes/products.php
@@ -0,0 +1,20 @@
+<?php
+
+// classe principale
+class Product {
+
+// proprietà
+public $name;
+public $category;
+public $quantity;
+public $price;
+
+// metodi
+public function __construct($name, $category, $quantity, $price) {
+    $this -> name = $name;
+    $this -> category = $category;
+    $this -> quantity = $quantity;
+    $this -> price = $price;
+}
+
+}
